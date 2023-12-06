bc8c2775505219cd07846f77ccde5d6430a76ccb
Neime <emilien.bouard@gmail.com>
2023-12-06 12:25:15 +0100

    day 6 : add typing

--- src/6/index.ts
+++ src/6/index.ts
@@ -2,45 +2,44 @@ import { readTextFile } from '../readfile';
 
 const [timesStr, distancesStr] = readTextFile('./6/input.txt');
 
-const times = timesStr.split(' ').filter(val => val).slice(1);
-const distances = distancesStr.split(' ').filter(val => val).slice(1);
+const times: string[] = timesStr.split(' ').filter(val => val).slice(1);
+const distances: string[] = distancesStr.split(' ').filter(val => val).slice(1);
 
-const races = times.map((time, index) => {
+const races: number[][] = times.map((time, index) => {
     return [
         parseInt(time),
         parseInt(distances[index])
     ]
 });
 
-const racesMaps = races.map((races, index) => {
+const racesMaps: Map<number, number>[] = races.map((races, index) => {
     const [maxTime, lastRecord] = races;
     
-    let map = new Map();
-    for (let i = 1; i < maxTime; i++) {
-        const timeHold = i;
-        const distanceRun = (maxTime - i) * i;
+    let map: Map<number, number> = new Map();
+    for (let i: number = 1; i < maxTime; i++) {
+        const timeHold: number = i;
+        const distanceRun: number = (maxTime - i) * i;
         if (distanceRun > parseInt(distances[index])) map.set(timeHold, distanceRun);
     }
     
     return map;
 });
 
-export const part1 = racesMaps.reduce((acc, raceMap, index) => {
+export const part1: number = racesMaps.reduce((acc, raceMap, index) => {
     return (acc === 0 ? 1 : acc) * raceMap.size;
 }, 0);
 
 
-const time = parseInt(times.reduce((acc, time) => {
+const time: number = parseInt(times.reduce((acc, time) => {
     return acc + time;
 }, ''));
-const distance = parseInt(distances.reduce((acc, time) => {
+const distance: number = parseInt(distances.reduce((acc, time) => {
     return acc + time;
 }, ''));
 
-let numberRaceWin = 0;
-for (let i = 1; i < time; i++) {
-    const timeHold = i;
-    const distanceRun = (time - i) * i;
+let numberRaceWin: number = 0;
+for (let i: number = 1; i < time; i++) {
+    const distanceRun: number = (time - i) * i;
     if (distanceRun > distance) numberRaceWin++;
 }
 
