eae11e4aae9bc08a0e5742c7e02c39a8b4256724
Neime <emilien.bouard@gmail.com>
2023-12-06 11:51:26 +0100

    day 6

--- src/6/index.ts
+++ src/6/index.ts
@@ -1,18 +1,48 @@
 import { readTextFile } from '../readfile';
 
-const lines = readTextFile('./6/test-part1.txt');
+const [timesStr, distancesStr] = readTextFile('./6/input.txt');
 
-export const part1 = lines.reduce<number>(
-    (acc, seed, index) => {
+const times = timesStr.split(' ').filter(val => val).slice(1);
+const distances = distancesStr.split(' ').filter(val => val).slice(1);
 
-    return acc;
+const races = times.map((time, index) => {
+    return [
+        parseInt(time),
+        parseInt(distances[index])
+    ]
+});
+
+const racesMaps = races.map((races, index) => {
+    const [maxTime, lastRecord] = races;
+    
+    let map = new Map();
+    for (let i = 1; i < maxTime; i++) {
+        const timeHold = i;
+        const distanceRun = (maxTime - i) * i;
+        if (distanceRun > parseInt(distances[index])) map.set(timeHold, distanceRun);
+    }
+    
+    return map;
+});
+
+export const part1 = racesMaps.reduce((acc, raceMap, index) => {
+    return (acc === 0 ? 1 : acc) * raceMap.size;
 }, 0);
 
-export const part2 = lines.reduce<number>(
-    (acc, seed, index) => {
 
-    return acc;
-}, 0);
+const time = parseInt(times.reduce((acc, time) => {
+    return acc + time;
+}, ''));
+const distance = parseInt(distances.reduce((acc, time) => {
+    return acc + time;
+}, ''));
+
+let numberRaceWin = 0;
+for (let i = 1; i < time; i++) {
+    const timeHold = i;
+    const distanceRun = (time - i) * i;
+    if (distanceRun > distance) numberRaceWin++;
+}
 
 console.log(`Day 6 Part 1: ${part1}`);
-console.log(`Day 6 Part 2: ${part1}`);''
+console.log(`Day 6 Part 2: ${numberRaceWin}`);''
