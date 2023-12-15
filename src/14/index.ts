8d3630f8e2e33ec82a2db295a218e4c52b63d5bb
Neime <emilien.bouard@gmail.com>
2023-12-15 12:52:44 +0100

    day 14 improve

--- src/14/index.ts
+++ src/14/index.ts
@@ -4,120 +4,138 @@ const grid: string[][] = readTextFileString('./14/input.txt').split('\n').map((l
 
 console.time('Execution Time');
 
-const print = (grid: string[][], roundRocksToPlace: RockPosition[]): void => {
-    const gridCopy: string[][] = grid.map((line: string[]) => [...line]);
-    for (let key in roundRocksToPlace) {
-        const rock: RockPosition = roundRocksToPlace[key];
-        gridCopy[rock.y][rock.x] = 'O';
-    }
-    console.log(gridCopy.map((line: string[]) => line.join('')).join('\n'));
+const print = (roundRocksToPlace: Map<string, Point>): void => {
+    const gridEmpty: string[][] = grid.map((line: string[]) => line.map((char: string) => char === 'O' ? '.' : char));
+    [...roundRocksToPlace.values()].forEach((rock: Point) => {
+        gridEmpty[rock.y][rock.x] = 'O';
+    });
+    console.log(gridEmpty.map((line: string[]) => line.join('')).join('\n'));
     console.log('\n');
 };
 
-type RockPosition = {
+type Point = {
     x: number;
     y: number;
 };
 
-let roundedRocksPosition: RockPosition[] = [];
+let roundedRocksPoint: Map<string, Point> = new Map();
+const roundRockAtStart: Map<string, Point> = new Map();
+let wallsPoint: Map<string, Point> = new Map();
 grid.forEach((line: string[], y: number) => {
     line.forEach((char: string, x: number) => {
         if (char === 'O') {
-            roundedRocksPosition[`${x},${y}`] = { x, y };
+            roundedRocksPoint.set(`${x},${y}`, { x, y });
+            roundRockAtStart.set(`${x},${y}`, { x, y });
         }
+        if (char === '#') wallsPoint.set(`${x},${y}`, { x, y });
     });
 });
 
-const gridWithoutRoundedRocks: string[][] = grid.map((line: string[]) => line.map((char: string) => char === 'O' ? '.' : char));
+const gridHeight = grid.length;
+const gridWidth = grid[0].length;
 
-const lastPlaceByColumnNorth: number[] = Array.from({ length: grid[0].length }, () => 0);
-const lastPlaceByColumnSouth: number[] = Array.from({ length: grid[0].length }, () => grid.length-1);
+const moveRock = (moveY: number, moveX: number): void => {
+        let hasMoved = true
+        while (hasMoved) {
+            hasMoved = false
+            roundedRocksPoint.forEach((rock: Point) => {
+                const targetX = rock.x + moveX;
+                const targetY = rock.y + moveY;
+                const targetKey = `${targetX},${targetY}`;
 
-const moveRock = (grid: string[][], direction: string): void => {
-    const lastPlaceByColumn: number[] = (direction === 'S') ? lastPlaceByColumnSouth : lastPlaceByColumnNorth;
-    for (let y: number = 0; y < grid.length; ++y) {
-        const lineIndex = (direction === 'S') ? grid.length-1-y : y;
-        const line: string[] = (direction !== 'E') ? grid[lineIndex] : [...grid[lineIndex]].reverse();
-        let lastPlaceByLine: number = (direction === 'E') ? line.length-1 : 0;
-        for (let x: number = 0; x < line.length; ++x) {
-            if ((direction === 'E')) x = (line.length-1)-x;
-            if (line[x] === '#') {
-                lastPlaceByColumn[x] = (direction === 'S') ? lineIndex-1 : lineIndex+1;
-                lastPlaceByLine = (direction === 'E') ? x-1 : x+1;
-            }
-
-            const roundedBlockAbove: RockPosition = roundedRocksPosition[`${x},${lineIndex}`];
-            if (roundedBlockAbove) {
-                const newPoint: RockPosition = {x:0, y:0};
-                switch (direction) {
-                    case 'N':
-                    case 'S':
-                        newPoint.y = lastPlaceByColumn[x];
-                        newPoint.x = roundedBlockAbove.x;
-                        break;
-                    case 'W':
-                    case 'E':
-                        newPoint.y = roundedBlockAbove.y;
-                        newPoint.x = lastPlaceByLine;
-                        break;
+                if (
+                    targetX >= 0 && targetY >= 0
+                    && targetX < gridWidth && targetY < gridHeight
+                    && !wallsPoint.has(targetKey)
+                    && !roundedRocksPoint.has(targetKey)
+                ) {
+                    hasMoved = true;
+                    roundedRocksPoint.delete(`${rock.x},${rock.y}`);
+                    rock.x = targetX;
+                    rock.y = targetY;
+                    roundedRocksPoint.set(targetKey, rock);
                 }
-
-                delete roundedRocksPosition[`${x},${lineIndex}`];
-                roundedRocksPosition[`${newPoint.x},${newPoint.y}`] = newPoint;
-                
-                (direction === 'S') ? lastPlaceByColumn[x]-- : lastPlaceByColumn[x]++;
-                (direction === 'E') ? lastPlaceByLine-- : lastPlaceByLine++;
-            }
+            });
         }
-    }
 };
 
-//print(gridWithoutRoundedRocks, roundedRocksPosition);
-moveRock(gridWithoutRoundedRocks, 'N');
-//print(gridWithoutRoundedRocks, roundedRocksPosition);
+//print(roundedRocksPoint);
+moveRock(-1, 0);
+//print(roundedRocksPoint);
 
 let total = 0;
-for (let key in roundedRocksPosition) {
-    const rock: RockPosition = roundedRocksPosition[key];
+[...roundedRocksPoint.values()].forEach((rock: Point) => {
     total += (grid.length - rock.y );
-}
+});
 console.log(`Day 14 Part 1: ${total}`);
 
 console.timeEnd('Execution Time');
 console.log('------');
 console.time('Execution Time');
 
-
-roundedRocksPosition = [];
+// init map
+roundedRocksPoint = new Map();
 grid.forEach((line: string[], y: number) => {
     line.forEach((char: string, x: number) => {
-        if (char === 'O') {
-            roundedRocksPosition[`${x},${y}`] = { x, y };
-        }
+        if (char === 'O') roundedRocksPoint.set(`${x},${y}`, { x, y });
     });
 });
 
 let cycles: number = 1000000000;
-while (cycles > 0) {
 
-    if (cycles % 100000 === 0) {
-        console.log(cycles);
-        console.timeEnd('Execution Time');
-        console.time('Execution Time');
+let i = 0;
+let cyclesPoint: string[] = [];
+let equalCycle: number = 0;
+let beforeCycleEqual: number = 0;
+print(roundedRocksPoint);
+while (i++ < cycles) {
+
+    moveRock(-1, 0); // north
+    let newCycle = [...roundedRocksPoint.values()].map((rock: Point) => `${rock.x},${rock.y}`).join('');
+    moveRock(0, -1); // west
+    newCycle += [...roundedRocksPoint.values()].map((rock: Point) => `${rock.x},${rock.y}`).join('');
+    moveRock(+1, 0); // south
+    newCycle += [...roundedRocksPoint.values()].map((rock: Point) => `${rock.x},${rock.y}`).join('');
+    moveRock(0, +1); // east
+    newCycle += [...roundedRocksPoint.values()].map((rock: Point) => `${rock.x},${rock.y}`).join('');
+    
+    // when cycle if equal to an existing cycle, we have found the recurring cycle number
+    if (cyclesPoint.find((cycle: string) => cycle === newCycle)) {
+        beforeCycleEqual = cyclesPoint.indexOf(newCycle)+1;
+        equalCycle = i-cyclesPoint.indexOf(newCycle)-1;
+        break;
     }
-
-    moveRock(gridWithoutRoundedRocks, 'N');
-    moveRock(gridWithoutRoundedRocks, 'W');
-    moveRock(gridWithoutRoundedRocks, 'S');
-    moveRock(gridWithoutRoundedRocks, 'E');
-    cycles--;
+    cyclesPoint.push(newCycle);
 }
 
 total = 0;
-for (let key in roundedRocksPosition) {
-    const rock: RockPosition = roundedRocksPosition[key];
+[...roundedRocksPoint.values()].forEach((rock: Point) => {
     total += (grid.length - rock.y );
+});
+
+// after beforeCycleEqual value cycles, each equalCycle it's the same result
+// so we can calculate the number of cycle to do before the result
+const numberCycleToResult = beforeCycleEqual + (1000000000-beforeCycleEqual)%equalCycle;
+// init map
+roundedRocksPoint = new Map();
+grid.forEach((line: string[], y: number) => {
+    line.forEach((char: string, x: number) => {
+        if (char === 'O') roundedRocksPoint.set(`${x},${y}`, { x, y });
+    });
+});
+
+i=0;
+while (i++ < numberCycleToResult) {
+    moveRock(-1, 0); // north
+    moveRock(0, -1); // west
+    moveRock(+1, 0); // south
+    moveRock(0, +1); // east
 }
+
+total = 0;
+[...roundedRocksPoint.values()].forEach((rock: Point) => {
+    total += (grid.length - rock.y );
+});
 export const part2 = total;
 
 console.log(`Day 14 Part 2: ${part2}`);
